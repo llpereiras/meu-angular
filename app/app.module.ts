8cb9be79b4097ed3fe8c44e496448f993ad32c14
Leandro Machado Pereira <llpereiras@gmail.com>
2016-11-25 13:02:16 -0200

    Importando e declarando novos componentes que foram adicionados

--- app/app.module.ts
+++ app/app.module.ts
@@ -5,10 +5,13 @@ import { routing } from './app.routes';
 
 import { AppComponent }  from './app.component';
 import { HomeComponent } from './home.component';
+import { PortfolioComponent } from './portfolio.component';
+import { ContatoComponent } from './contato.component';
+import { ViewComponent } from './view.component';
 
 @NgModule({
   imports: [ BrowserModule, routing ],
-  declarations: [ AppComponent, HomeComponent],
+  declarations: [ AppComponent, HomeComponent, PortfolioComponent, ContatoComponent, ViewComponent],
   bootstrap: [ AppComponent ]
 })
 export class AppModule { }
