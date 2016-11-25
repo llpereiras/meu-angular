dea41861feec74c4590910268bba888ab0208287
Leandro Machado Pereira <llpereiras@gmail.com>
2016-11-25 13:18:46 -0200

    Adicionando componente e view home

--- app/home.component.ts
+++ app/home.component.ts
@@ -1,9 +1,17 @@
 import { Component } from '@angular/core';
 
 @Component({
-  selector: 'home-list',
-  template: `
-  <h1> Home </h1>
-  `
+  moduleId: module.id,
+  selector: 'app-home',
+  styles: [`
+    .ilikeangular2 {
+      width: 50px;
+      height: 50px;
+      background-repeat: no-repeat;
+      background-image: url("app/free_bt_theme/img/happy.png");
+      display: inline-block;
+    }
+  `],
+  templateUrl: '../../views/home.html'
 })
 export class HomeComponent{}
