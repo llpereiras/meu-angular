93ae98dc5aa619b23d17b5e273a068d952d1e743
Leandro Machado Pereira <llpereiras@gmail.com>
2016-11-25 13:02:44 -0200

    Adicionando novas rotas

--- app/app.routes.ts
+++ app/app.routes.ts
@@ -1,16 +1,22 @@
 import { Routes, RouterModule } from '@angular/router';
 
 import { HomeComponent } from './home.component';
+import { ContatoComponent } from './contato.component';
+import { PortfolioComponent } from './portfolio.component';
+import { ViewComponent } from './view.component';
 
 const routes: Routes = [
   {
-    path: 'home',
-    component: HomeComponent,
+    path: '',
+    component: HomeComponent
   },
   {
-    path: '',
-    redirectTo: '/home',
-    pathMatch: 'full'
+    path: 'portfolio',
+    component: PortfolioComponent
+  },
+  {
+    path: 'contato',
+    component: ContatoComponent
   },
 ];
 
