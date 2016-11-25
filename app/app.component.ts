bd49458b29e4f766f1270f73bf2458fc7e4ed12b
Leandro Machado Pereira <llpereiras@gmail.com>
2016-11-25 18:31:56 -0200

    Retirando arquivos desnecessários

--- app/app.component.ts
+++
@@ -1,9 +0,0 @@
-import { Component } from '@angular/core';
-
-@Component({
-  selector: 'my-app',
-  templateUrl: '../views/layout.html'
-})
-export class AppComponent {
-  title:string = 'Leandro Machado Pereira';
-}
--- app/portfolio.component.ts
+++
@@ -1,9 +0,0 @@
-import { Component } from '@angular/core';
-import { AppComponent } from './app.component'
-
-@Component({
-  moduleId: module.id,
-  selector: 'app-portfolio',
-  templateUrl: '../../views/portfolio.html'
-})
-export class PortfolioComponent{}
